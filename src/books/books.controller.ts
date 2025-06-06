86f63e4e05a30029c93dba368cdde188b14f6ad1
akshats-nova <akshats@novatab.com>
2025-06-06 12:41:44 +0530

    Integrated MongoDB

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 
-import { HttpCode, HttpStatus, BadRequestException, Controller, ParseIntPipe, Query, ValidationPipe, Body, Get, Post, Param, Patch, Delete } from '@nestjs/common';
+import { HttpCode, HttpStatus, BadRequestException, Controller, Query, ValidationPipe, Body, Get, Post, Param, Put, Delete } from '@nestjs/common';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 import { BooksService } from './books.service';
@@ -10,7 +10,7 @@ export class BooksController {
    constructor( private readonly booksService: BooksService) {}
    
    @Get()
-   findAll(
+   async findAll(
     @Query('genre') genre?: 'FICTION' | 'NON-FICTION' | 'HISTORY' | 'SELF-HELP', 
     @Query('isAvailable') isAvailable?: string,
     @Query('sortBy') sortBy?: 'title' | 'author' | 'publishedDate') {
@@ -24,23 +24,23 @@ export class BooksController {
         return this.booksService.findAll(genre, availability, sortBy);
    }
    @Get(':id')
-   findOne(@Param('id', ParseIntPipe) id:number){
-        return this.booksService.findOne(id)
+   async findOne(@Param('id') id:string){
+        return this.booksService.findOne(id);
    }
 
    @Post()
    @HttpCode(HttpStatus.CREATED)
-   create(@Body(ValidationPipe) createBookDto: CreateBookDto){
+   async create(@Body(ValidationPipe) createBookDto: CreateBookDto){
     return this.booksService.create(createBookDto);
    }
 
-   @Patch(':id')
-   update(@Param('id', ParseIntPipe) id:number, @Body(ValidationPipe) updateBookDto: UpdateBookDto){
+   @Put(':id')
+   async update(@Param('id') id:string, @Body() updateBookDto: UpdateBookDto){
         return this.booksService.update(id, updateBookDto);
    }
 
     @Delete(':id')
-    delete(@Param('id', ParseIntPipe) id:number){
+    async delete(@Param('id') id:string){
         return this.booksService.delete(id)
     }
 
